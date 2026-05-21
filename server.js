dd34e32b04df0646d4d21067e0f62edd8228d9c2
DavidRF16 <david_romero_m-tsapp1ma2425@cev.com>
2026-05-21 14:33:32 +0200

    por favor

--- server.js
+++ server.js
@@ -8,10 +8,8 @@ const __dirname = path.dirname(__filename);
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Servir archivos estáticos desde /dist
 app.use(express.static(path.join(__dirname, "dist")));
 
-// Manejar rutas SPA
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
